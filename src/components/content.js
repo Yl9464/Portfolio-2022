3603946cf44912394d4cf0032d9f0f8389b53cc9
Ying <yinglu2641@gmail.com>
2022-10-02 14:52:24 -0400

    adding icons working on their layout next

--- src/components/content.js
+++ src/components/content.js
@@ -1,3 +1,5 @@
+import Skills from "./skills"
+
 function Content() {
     return (
 
@@ -12,7 +14,7 @@ function Content() {
             </div>
             <div className="skills">
                 <h4>Skills</h4>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                <Skills />
             </div>
             <div className="contactMe">
                 <h4 id="contact">Contact me</h4>
---
+++ src/components/skills.js
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Skills() {
+    return (
+        <div className='icons'>
+            <FontAwesomeIcon className='jsIcon' icon="fa-brands fa-js" size="6x" />
+            <FontAwesomeIcon icon="fa-brands fa-css3-alt" size="6x" />
+            <FontAwesomeIcon icon="fa-brands fa-node" size="6x" />
+            <FontAwesomeIcon icon="fa-brands fa-github" size='6x' />
+            <FontAwesomeIcon icon="fa-brands fa-html5" size="6x"/>
+            <FontAwesomeIcon icon="fa-brands fa-react" size="6x" />
+
+        </div>
+    )
+}
+export default Skills
