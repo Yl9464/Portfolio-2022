b439e8181fc01cd71c4e0700053b677e63963fe1
Ying <yinglu2641@gmail.com>
2022-10-04 17:44:25 -0400

    add all routes, started resume page, reformatted contact form

--- src/components/header.js
+++ src/components/header.js
@@ -5,16 +5,15 @@ function Header() {
         <div className="header_section">
             <nav id="menubar" class="navbar navbar-expand-lg custom_nav-container ">
                 <ul class="navbar-nav">
-
-
                     <li class="nav-item active">
-                        <a class="nav-link" href="about"> About</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link" href="skills">Skills<span class="sr-only"></span></a>
+                        <a class="nav-link" href="about">
+                            <Link exact to="/">Home</Link>
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="portfolio.html">Resume</a>
+                        <a class="nav-link" href="portfolio.html">
+                            <Link to="/resume">Resume</Link>
+                        </a>
                     </li>
         
                     <li class="nav-item">
