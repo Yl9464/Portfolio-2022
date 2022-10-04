cf745218be0184d16a047aa59a64760071a3f24a
Ying <yinglu2641@gmail.com>
2022-10-04 15:54:08 -0400

    creating contact form with functionality. working on styling next

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}) )
-
+app.use(express.static("portfolio-frontend/build"))
 app.listen(3000, () => {
     console.log("listening on port 3000")
 })
