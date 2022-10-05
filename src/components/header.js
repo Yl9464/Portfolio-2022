f08a89dfabf284173eb6aeb23431eb8661c59c91
Ying <yinglu2641@gmail.com>
2022-10-05 12:02:05 -0400

    restyling navbar

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,34 @@ import {  Link } from "react-router-dom"
 
 function Header() {
     return (
-        <div className="header_section">
-            <nav id="menubar" class="navbar navbar-expand-lg custom_nav-container ">
-                <ul class="navbar-nav">
+        <div className="header-section">
+             <nav class="navbar navbar-expand-lg">
+            <span class="navbar-brand">Ying Lu</span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="about">
+                        <a class="nav-link" href="home">
                             <Link exact to="/">Home</Link>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="portfolio.html">
+                        <a class="nav-link" href="resume">
                             <Link to="/resume">Resume</Link>
                         </a>
                     </li>
-        
                     <li class="nav-item">
                         <a class="nav-link" href="contact">
                             <Link to="/contact">Contact</Link>
                         </a>
                      </li>
                     </ul>
-            </nav>
-            
-        </div>
-           
+                    </div>
+                </nav>
+            </div> 
     )
 }
 export default Header
