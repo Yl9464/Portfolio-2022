747ea2d76a7f34c4c1d50b0352be2b7f39974a50
Ying <yinglu2641@gmail.com>
2022-10-07 16:19:57 -0400

    adding contactPage folder and contactFrom file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from "./components/home"
-import Contact from "./components/contact"
+import Contact from "./components/contactPage/contact"
 import Resume from "./components/resume/resume"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
