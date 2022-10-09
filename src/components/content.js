0cd9dcf48f3aa91ea357c84cadc4b5a693f43c16
Ying <yinglu2641@gmail.com>
2022-10-09 11:57:57 -0400

    updating adbout me and replacing fun facts with Aspiration section

--- src/components/content.js
+++ src/components/content.js
@@ -5,21 +5,23 @@ function Content() {
             <div className="aboutMe">
                 <h4>About me</h4>
                 <p>
-                    I am a passionate, driven, and committed individual who is tenacious in the face of adversity.Transitioning from a dog trainer background,
-                    I bring a diverse perspective to the tech world along with the patience and devotion that brought me success in previous roles.Having completed 
-                    Framingham State University's Web Development certificate program, I now look forward to applying my coding skills in meaningful way that will make a 
-                    lasting and positive impact on society.                                                           
+                    I am a Junior Full Stack developer and received a certification in web development from Framingham State University.
+                    During my program I realized my unique mindset allowed me to view problems in a way that could establish resolutions
+                    in effective and creative ways. My entire life I have loved spending time and working with dogs, so I took my lifelong
+                    passion and became a dog trainer. After 3 years in the field, I had accomplished as much I could from that world and
+                    decided to move onto to something more challenging that could better utilize my skills. During my time at Framingham State,
+                    I realized the many parallels between dog training and coding. They both require perseverance, motivation, an adaptive mindset, 
+                    and a strong passion for the role. As I make the transition into the tech world, I bring a diverse perspective along with the patience
+                     and devotion that brought me success as a dog trainer.                                       
                 </p>
             </div>
-            <div className="funFacts">
-                <h4>Fun Facts!</h4>
-                <ol>
-                    <li> Morbi eget turpis a velit finibus ultrices et vel justo.</li>
-                    <li> Maecenas porttitor felis quis enim facilisis cursus.</li>
-                    <li> Proin vel sapien quis justo aliquam interdum.</li>
-                    <li> Donec efficitur lorem tristique dolor posuere, vel semper neque tempor.</li>
-                    <li> Proin vitae justo bibendum felis faucibus lobortis non eget odio.</li> 
-               </ol>
+            <div className="aspirations">
+                <h4>My Aspirations:</h4>
+                <p>
+                    Technology is the future; every day new and amazing products are being developed to strengthen and advance society.
+                    My goal is to be a part of the teams who are creating these cutting-edge technologies, between my professional skills
+                    and strong mindset I know I will reach that goal.
+                </p>
             </div> 
             <Skills />
         </div> 
