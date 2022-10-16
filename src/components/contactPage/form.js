cd6bb79549b4c90afa71bfabe0922770ada3bf87
Ying <yinglu2641@gmail.com>
2022-10-16 13:52:55 -0400

    adding required to all fields

--- src/components/contactPage/form.js
+++ src/components/contactPage/form.js
@@ -6,18 +6,19 @@ function FormFunctionality() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
-    const [formStatus,setFormStatus] = useState('')
+    const [formStatus, setFormStatus] = useState('')
 
     const form = useRef();
 
     const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
-    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_I
     const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY
-
-    // const handleChange = (e) => {
-    //     const { name, email, message, value } = e.target
-    //     setClearForm((prevState) => ({ ...prevState, [name]: value, [email]: value, [message]: value }))
-    // }
+    
+    function clearForm () {
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -25,41 +26,47 @@ function FormFunctionality() {
         emailjs.sendForm(serviceId, templateId, form.current, publicKey)
             .then((result) => {
                 console.log(result.text);
-        
                 setFormStatus('Email sent successfully! I will be in touch shortly 😁')
-               
-                setName('')
-                setEmail('')
-                setMessage('')
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+                clearForm()
 
+            }).then(error => {
+                console.log(error)
+                clearForm()
+            })
+        
+
+    }
+           
     return (
         <div>
-            <Form ref={form} onSubmit={sendEmail}>
+            <Form  ref={form} onSubmit={sendEmail}>
                 <Row className="mb-3">
                 <Col md lg="3">
-                    <Form.Group as={Col} controlId="formGridName" >
+                        <Form.Group as={Col} controlId="validationCustom03" >
                         <Form.Label>Name</Form.Label>
                             <Form.Control
                                 type="text"
                                 name="user_name"
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
-                                placeholder="Full Name" />
+                                placeholder="Full Name" 
+                                required= {true}
+                                />
+                       
                         </Form.Group>
                     </Col>
                     <Col md lg="3">
-                        <Form.Group as={Col} >
+                        <Form.Group as={Col} controlId="validationCustom04">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
                                 type="email"
                                 name="user_email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Email" />
+                                placeholder="Email" 
+                                required={true}
+                                />
+                          
                         </Form.Group>
                     </Col>
                     </Row>
@@ -71,7 +78,8 @@ function FormFunctionality() {
                             name="message"
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
-                            style={{ height: '110px', width: "50%" }} />
+                            style={{ height: '110px', width: "50%" }}
+                            required={true}/>
                         </Form.Group>
 
             <Button type="submit">Submit </Button>
