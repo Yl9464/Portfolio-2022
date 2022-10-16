3817c5f10ff5461c943200407ee522f9e6c4fa16
Ying <yinglu2641@gmail.com>
2022-10-16 11:23:27 -0400

    console loging form values

--- src/components/contactPage/form.js
+++ src/components/contactPage/form.js
@@ -3,25 +3,28 @@ import { Button, Col, Form, Row} from 'react-bootstrap';
 import emailjs from "@emailjs/browser"
 
 function FormFunctionality() {
-
-    const initialState = {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [state, setState] = useState({
         name: '',
         email: '',
-        message: ''
-    }
-    const [{ name, email, message }, setState] = useState(initialState);
-    const [statusMessage, setStatusMessage] = useState("");
+        message: '',
+    })
+    const [formStatus, setFormStatus] = useState("");
 
     const handleChange = (e) => {
-        const {name, value} = e.target
-        setState((prevState) => ({ ...prevState, [name]: value }))
-            
+        const {name, email, message, value } = e.target
+        setState((prevState) => ({ ...prevState, [name]: value, [email]: value, [message]: value }))      
     }
-    const clearState = () => setState({ ...initialState })
+
+    const clearState = () => setState(state)
     
     const handleSubmit = (e) => {
         e.preventDefault()
+        
         console.log(name, email, message);
+        
         const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_I
         const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
         const userId = process.env.REACT_APP_EMAILJS_PUBLIC_KEY
@@ -37,44 +40,60 @@ function FormFunctionality() {
                 (result) => {
                     console.log(result.text, result.status);
                     clearState()
-                    setStatusMessage("Email sent successfully!")
+                    setFormStatus("Email sent successfully!")
                 },
                 (error) => {
                     console.log(error.text);
-                    setStatusMessage("Sorry, email was not sent because an error has occured")
+                    setFormStatus(`${error.text} happened`)
                 }
         )
     }
     
     return (
-        <Form onSubmit={handleSubmit} >
+        <div> 
+        <Form onSubmit={handleSubmit}>
             <Row className="mb-3">
                 <Col md lg="3">
                     <Form.Group as={Col} controlId="formGridName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" value={name} placeholder="Full Name" />
+                        <Form.Control
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Full Name" />
                     </Form.Group>
                 </Col>
                 <Col md lg="3">
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" value={email} placeholder="Email" />
+                        <Form.Control
+                            type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email" />
                     </Form.Group>
                 </Col>
             </Row>
 
             <Form.Group className="mb-3" controlId="ControlTextarea1">
                 <Form.Label>Your messesage</Form.Label>
-                <Form.Control as="textarea" value={message} style={{ height: '110px', width: "50%" }}
+                <Form.Control
+                    as="textarea"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    style={{ height: '110px', width: "50%" }}
                 />
             </Form.Group>
 
-            <Button type="submit" onClick={() => handleChange}>
+            <Button
+                type="submit"
+                id="submitForm"
+                onClick={() => handleChange}>
             Submit
             </Button>
-            <p>{statusMessage}</p> 
-
-        </Form>
+            </Form>
+            <p>{formStatus}</p> 
+            </div>
     )
 }
 export default FormFunctionality 
